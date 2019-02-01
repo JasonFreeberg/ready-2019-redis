a2a6133666011147d8fb1e1754e0fd806af9e25a
Jason Freeberg <jr.freeberg@comcast.net>
2019-02-01 11:18:06 -0800

    Removed annotations

--- src/main/java/com/example/RedisCacheApplication.java
+++ src/main/java/com/example/RedisCacheApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableCaching
 @SpringBootApplication
 public class RedisCacheApplication {
     public static void main(String[] args) {
